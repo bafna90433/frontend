c2116a858b773e321fc886955eb42c194b02386d
bafna90433 <bafna90433@gmail.com>
2026-02-19 19:03:56 +0530

    Fixed CORS issue by adding server hostname

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,13 @@ const config: CapacitorConfig = {
   appId: "com.bafnatoys.app",
   appName: "Bafna Toys",
   webDir: "dist",
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  
+  // 👇 Ye CORS fix karne wala naya block hai 👇
+  server: {
+    hostname: "bafnatoys.com",
+    androidScheme: "https"
+  }
 };
 
 export default config;
