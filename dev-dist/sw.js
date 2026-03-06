c7e47c7d0694970079c69d023cbb6311d0d2a01a
bafna90433 <bafna90433@gmail.com>
2026-03-06 19:24:30 +0530

    Updated Home with cute pastel design and slider arrows

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.g2vjt9spnlo"
+    "revision": "0.lu2bamb43ak"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
