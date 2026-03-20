7127258133b64ce32f89605b53b5152796328f33
bafna90433 <bafna90433@gmail.com>
2026-03-20 20:18:30 +0530

    Fix product card image issue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.a05skdldpvc"
+    "revision": "0.r9kqki9idq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
