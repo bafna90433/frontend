2f15751f96813fec7afefb809d573a2ac6478d2a
bafna90433 <bafna90433@gmail.com>
2026-03-20 18:55:32 +0530

    Fix iPhone product image overflow issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-
-// ✅ YAHAN CSS IMPORT KARNI ZAROORI HAI
-import "./styles/index.css"; 
-
 import App from "./App";
 
 const container = document.getElementById("root");
