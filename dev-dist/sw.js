0e7679cc6b5a446c2eac5c5089c0fc0b9fe5954b
bafna90433 <bafna90433@gmail.com>
2026-03-25 20:27:10 +0530

    Update index and add favicon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.rrik8kv9is4"
+    "revision": "0.4udqt5od428"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
