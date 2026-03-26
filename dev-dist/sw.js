b3bd3c7e83b083439bce093e7aa94a7e62190d27
bafna90433 <bafna90433@gmail.com>
2026-03-26 12:32:54 +0530

    Updated favicon images and meta tags for Google

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.309ogmjl5q"
+    "revision": "0.u1g3snj6mg4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
